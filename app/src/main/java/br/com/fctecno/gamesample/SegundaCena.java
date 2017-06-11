13f46b84edeec7087816ebd3bd94c1ffb5c058d6
Tiago F. Costa <tgfarias@posmobile.com>
2017-06-11 06:21:30 -0300

    Adicionando bulldog caminhando

--- app/src/main/java/br/com/fctecno/gamesample/SegundaCena.java
+++ app/src/main/java/br/com/fctecno/gamesample/SegundaCena.java
@@ -2,13 +2,14 @@ package br.com.fctecno.gamesample;
 
 import br.com.fctecno.andgraph.AGGameManager;
 import br.com.fctecno.andgraph.AGScene;
+import br.com.fctecno.andgraph.AGScreenManager;
 import br.com.fctecno.andgraph.AGSprite;
 
 /**
  * Created by catolica2017 on 10/06/17.
  */
 public class SegundaCena extends AGScene {
-
+    AGSprite bulldog = null;
 
     SegundaCena(AGGameManager gerenteJogo){
         super(gerenteJogo);
@@ -16,7 +17,15 @@ public class SegundaCena extends AGScene {
 
     @Override
     public void init() {
+        setSceneBackgroundColor(0.0f, 0.0f, 0.0f);
 
+        bulldog = createSprite(R.mipmap.ic_budogg, 4, 4);
+        bulldog.setScreenPercent(20, 40);
+        bulldog.vrPosition.setX((AGScreenManager.iScreenWidth - bulldog.getSpriteWidth())/2);
+        bulldog.vrPosition.setY(AGScreenManager.iScreenHeight / 2);
+        bulldog.addAnimation(10, true, 0, 11);
+
+        bulldog.iMirror = AGSprite.HORIZONTAL;
     }
 
     @Override
